9a891e2142e28529def2f931708f82e11531c641
EnergyCrafter <ispete1@outlook.com>
2017-11-03 19:58:30 -0500

    changed opendb.php

--- opendb.php
+++ opendb.php
@@ -1,7 +1,7 @@
 <?php
 // opens the database connection
-$conn = mysqli_connect($DATABASE_IP, $DATABASE_USERNAME, $DATABASE_PASSWORD) or die ('Error connecting to mysql');
+$conn = mysqli_connect($DATABASE_IP, $DATABASE_USERNAME, $DATABASE_PASSWORD, $DATABASE_NAME) or die ('Error connecting to mysql');
 
 // connects to the specific database on the MySQL server
-mysqli_select_db($conn, $DATABASE_NAME);
+//mysqli_select_db($conn, $DATABASE_NAME);
 ?>
